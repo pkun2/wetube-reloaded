09b695a13ed10958121eeb3f6f962bb0c36cbc92
unknown <pkun2@naver.com>
2021-08-02 18:37:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> home
+/join -> Join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit user
+/user/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a video
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 80;
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => 
+    console.log(`✅ Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
