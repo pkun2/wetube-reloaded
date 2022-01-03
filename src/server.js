d0d8b81222d6596cd76c3d1cb0d9f9f255af60ff
pkun2 <pkun2@naver.com>
2022-01-03 21:28:12 +0900

    connecting DB and Make Video Model

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+
 
 const PORT = 4885;
 
