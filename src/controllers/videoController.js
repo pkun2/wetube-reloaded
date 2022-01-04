3799deb9cd96490d753aa5004abfaaaea79e0468
pkun2 <pkun2@naver.com>
2022-01-04 18:07:03 +0900

    add async,await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,49 +1,20 @@
-import { redirect } from "express/lib/response";
+import Video from "../models/Video";
 
-let videos = [
-    {
-        title: "First Video",
-        rating:5,
-        comment:2,
-        createAt: "2 minutes ago",
-        views:1,
-        id:1
-    },
-    {
-        title: "Second Video",
-        rating:5,
-        comment:2,
-        createAt: "2 minutes ago",
-        views:59,
-        id:2
-    },
-    {
-        title: "Third Video",
-        rating:5,
-        comment:2,
-        createAt: "2 minutes ago",
-        views:59,
-        id:3
-    },
-];
-
-export const trending = (req, res) => {
+export const home = async(req, res) => {
+    const videos  = await Video.find({})   
     return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+    return res.render("watch", {pageTitle: `Watching`});
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle:`Editing:`});
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -52,14 +23,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
     const {title} = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comment: 0,
-        createAt: "just now",
-        views: 0,
-        id: videos.length + 1,
-    };
-    videos.push(newVideo);
     return res.redirect("/");
 };
